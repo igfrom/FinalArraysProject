ac355c7da8b11afb435528790e0616f3f1c9fc90
Igor From <ig.from@yandex.ru>
2023-01-07 18:41:29 +0700

    final

--- Program.cs
+++ Program.cs
@@ -12,22 +12,22 @@ Console.Write("Введите количество элементов ");
 int size = Convert.ToInt32(Console.ReadLine());
 
 string[] arrayString = new string[size];
-for (int i = 0; i < size; i++)
+for (int i = 0; i < size; i++) // цикл для введения в консоль элементов
 {
     Console.WriteLine($"Введите {i+1}-й элемент: ");
     string element = Convert.ToString(Console.ReadLine());
     arrayString[i] = element;
 }
 
-string[] arrayFinal = new string[size];
+string[] arrayFinal = new string[size]; 
 int maxlength = 3;
 int pos = 0;
 
-for (int j = 0; j < size; j++)
+for (int j = 0; j < size; j++) // цикл для фильтрации элементов <= 3 символам.
 {
-    if (arrayStrings[j].Length <= maxlength)
+    if (arrayString[j].Length <= maxlength)
     {
-        arrayFinal[pos] = arrayStrings[j];
+        arrayFinal[pos] = arrayString[j];
         pos++;
     }
 }
